f524f4cfc19c36620c6c11713137433aafb005c8
tallpizza <tallpizza@naver.com>
2024-02-19 22:38:09 +0900

    update pdf file

---
+++ app/Sample.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useCallback, useMemo, useState } from "react";
+import { useResizeObserver } from "@wojtekmaj/react-hooks";
+import { pdfjs, Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+import "./Sample.css";
+
+import type { PDFDocumentProxy } from "pdfjs-dist";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const resizeObserverOptions = {};
+
+const maxWidth = 800;
+
+type PDFFile = string | File | null;
+
+export default function Sample() {
+  const [file, setFile] = useState<PDFFile>("");
+  const [numPages, setNumPages] = useState<number>();
+  const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+
+  const options = useMemo(
+    () => ({
+      cMapUrl: "/cmaps/",
+      standardFontDataUrl: "/standard_fonts/",
+    }),
+    []
+  );
+
+  const onResize = useCallback<ResizeObserverCallback>((entries) => {
+    const [entry] = entries;
+
+    if (entry) {
+      setContainerWidth(entry.contentRect.width);
+    }
+  }, []);
+
+  useResizeObserver(containerRef, resizeObserverOptions, onResize);
+
+  function onFileChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    const { files } = event.target;
+
+    if (files && files[0]) {
+      setFile(files[0] || null);
+    }
+  }
+
+  function onDocumentLoadSuccess({
+    numPages: nextNumPages,
+  }: PDFDocumentProxy): void {
+    setNumPages(nextNumPages);
+  }
+
+  return (
+    <div className="Example">
+      <div className="Example__container">
+        <div className="Example__container__load">
+          {/* <label htmlFor="file">Load from file:</label> */}
+          <input onChange={onFileChange} type="file" />
+        </div>
+        <div className="Example__container__document" ref={setContainerRef}>
+          <Document
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            options={options}
+          >
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                width={
+                  containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
+                }
+              />
+            ))}
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.externals.push({ canvas: "commonjs canvas" });
+    return config;
+  },
+};
 
 export default nextConfig;
